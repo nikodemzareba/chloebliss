a76d6f67da0c3dc6ea749f153c6e74cb4c003c5e
Niko <nz76@kent.ac.uk>
2023-09-24 02:17:11 +0100

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'katrinasten.com',
+  title: 'chloebliss.com',
   // description: 'Welcome to Katrina Sten OnlyFan Page',
 }
 
